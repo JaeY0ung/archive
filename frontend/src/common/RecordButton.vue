1dc4f15452e5525dfdf74e2f434c3527980a4e6a
신순호 <ssh9234@gmail.com>
2024-07-24 14:41:39 +0900

    feat: 악보 플레이매니저

--- frontend/src/common/RecordButton.vue
+++ frontend/src/common/RecordButton.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive,watch} from "vue";
+
+const props = defineProps({
+  triggerSplit: Boolean,
+});
 
 const isRecording = ref(false);
 const mediaRecorder = ref(null);
@@ -9,73 +13,93 @@ let startTime = 0;
 
 // 시작 버튼을 눌렀을때 작동하는 함수
 const startRecording = async () => {
-    // 기존 블롭 초기화
-    audioBlobs.value = []; 
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  // 기존 블롭 초기화
+  audioBlobs.value = [];
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    //* ! MediaRecorder 초기화
-    mediaRecorder.value = new MediaRecorder(stream);
+  //* ! MediaRecorder 초기화
+  mediaRecorder.value = new MediaRecorder(stream);
 
-    // ondataavailabe => 이벤트가 발생됐을 때 실행되는 함수
-    mediaRecorder.value.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-            chunks.push(event.data);
-        }
-    };
-    
-    // 녹음기가 종료됐을때 다시 실행(3초마다 무한반복)
-    mediaRecorder.value.onstop = () => {
-        if (chunks.length > 0) {
-            const blob = new Blob(chunks, { type: 'audio/webm' }); // Binary Large Object (음악 파일)  
-            const url = URL.createObjectURL(blob); // Url 생성 (나중에 파일 전송으로 수정해야함)
-            audioBlobs.value.push({ blob, url }); // 
-            chunks.length = 0; // 청크 초기화
-        }
-        // 녹음을 다시 시작합니다.
-        if (isRecording.value) {
-            mediaRecorder.value.start();
-            splitRecording();
-        }
-    };
+  // ondataavailabe => 이벤트가 발생됐을 때 실행되는 함수
+  mediaRecorder.value.ondataavailable = (event) => {
+    if (event.data.size > 0) {
+      chunks.push(event.data);
+    }
+  };
 
-    mediaRecorder.value.start();
-    isRecording.value = true;
-    startTime = performance.now();
-    splitRecording(); // 3초뒤에 끊어서 저장하고 stop()호출 => 위에있는 onstop이 작동해 무한반복한다
+  // 녹음기가 종료됐을때 다시 실행(3초마다 무한반복)
+  mediaRecorder.value.onstop = () => {
+    if (chunks.length > 0) {
+      const blob = new Blob(chunks, { type: "audio/webm" }); // Binary Large Object (음악 파일)
+      const url = URL.createObjectURL(blob); // Url 생성 (나중에 파일 전송으로 수정해야함)
+      audioBlobs.value.push({ blob, url }); //
+      chunks.length = 0; // 청크 초기화
+    }
+    // 녹음을 다시 시작합니다.
+    if (isRecording.value) {
+      mediaRecorder.value.start();
+      //    splitRecording();
+    }
+  };
+
+  mediaRecorder.value.start();
+  isRecording.value = true;
+  startTime = performance.now();
+  //splitRecording(); // 3초뒤에 끊어서 저장하고 stop()호출 => 위에있는 onstop이 작동해 무한반복한다
+  //splitRecording2();
 };
 
 // 녹음기를 3초마다 초기화시켜주는 함수
 const splitRecording = () => {
-    setTimeout(() => {
-        if (isRecording.value && mediaRecorder.value.state === 'recording') {
-        mediaRecorder.value.stop();
-        }
-    }, 3000);
+  setTimeout(() => {
+    if (isRecording.value && mediaRecorder.value.state === "recording") {
+      mediaRecorder.value.stop();
+    }
+  }, 3000);
+};
+const splitRecording2 = () => {
+  if (isRecording.value && mediaRecorder.value.state === "recording") {
+    mediaRecorder.value.stop();
+  }
 };
 
 // 녹음 중에 stop->start 상태가 무한반복이기 때문에 종료 조건을 알려줘야 한다.
 const stopRecording = () => {
-    isRecording.value = false;
-    if (mediaRecorder.value.state === 'recording') {
-        mediaRecorder.value.stop();
-    }
+  isRecording.value = false;
+  if (mediaRecorder.value.state === "recording") {
+    mediaRecorder.value.stop();
+  }
 };
+
+// watch를 사용하여 props 변경 감지 및 함수 실행
+watch(() => props.triggerSplit, (newValue) => {
+  if (newValue) {
+    splitRecording2();
+  }
+});
+
 </script>
 
 <template>
-    <div>
-        <button @click="startRecording" :disabled="isRecording">Start Recording</button>
-        <button @click="stopRecording" :disabled="!isRecording">Stop Recording</button>
-        <ul>
-            <li v-for="(blob, index) in audioBlobs" :key="index">
-            <a :href="blob.url" :download="`audio-part-${index + 1}.webm`">Download Part {{ index + 1 }}</a>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <button @click="startRecording" :disabled="isRecording">
+      Start Recording
+    </button>
+    <button @click="stopRecording" :disabled="!isRecording">
+      Stop Recording
+    </button>
+    <ul>
+      <li v-for="(blob, index) in audioBlobs" :key="index">
+        <a :href="blob.url" :download="`audio-part-${index + 1}.webm`"
+          >Download Part {{ index + 1 }}</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 button {
-    margin: 5px;
+  margin: 5px;
 }
 </style>
--- frontend/src/common/sheet/Sheet.vue
+++ frontend/src/common/sheet/Sheet.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="margin-left :100px">
+  <div style="margin-left: 100px">
     <div>
       <button @click="play">재생</button>
       <button @click="pause">일시정지</button>
@@ -7,19 +7,20 @@
       <label for="volumeSlider">Volume: {{ volume }}</label>
       <input id="volumeSlider" type="range" min="0" max="100" v-model="volume" @input="setVolume">
     </div>
-    <div id="scrollContainer" style="overflow-y:scroll" :style="{width: width+'px', height:height+'px'}">
-      <div id="osmdContainer" ref="osmdContainer" ></div>
+    <div id="scrollContainer" style="overflow-y: scroll" :style="{ width: width + 'px', height: height + 'px' }">
+      <div id="osmdContainer" ref="osmdContainer"></div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 
 const props = defineProps({
-  width : Number,
-  height : Number,
-})
+  width: Number,
+  height: Number,
+});
+const emit = defineEmits(['measure-changed']);
 
 const osmdContainer = ref(null);
 const volume = ref(50); // 초기 볼륨 값을 50으로 설정
@@ -33,57 +34,68 @@ const loadMusicXML = async () => {
 };
 
 const setupPlaybackManager = () => {
-  console.log("setup PlaybackManager");
+  console.log('setup PlaybackManager');
   const timingSource = new window.opensheetmusicdisplay.LinearTimingSource();
-  console.log("init timingSource");
+  console.log('init timingSource');
   playbackManager = new window.opensheetmusicdisplay.PlaybackManager(
     timingSource,
     undefined,
     new window.opensheetmusicdisplay.BasicAudioPlayer(),
     undefined
   );
-  console.log("init playbackManager");
+  console.log('init playbackManager');
   playbackManager.DoPlayback = true;
   playbackManager.DoPreCount = false;
   playbackManager.PreCountMeasures = 1;
   osmd.FollowCursor = true;
-  // 기본 컨트롤패널 대신 내가 커스터마이징 한 버튼 사용
-  // const playbackControlPanel = new window.opensheetmusicdisplay.ControlPanel();
-  // playbackControlPanel.addListener(playbackManager);
-  // playbackControlPanel.addListener({
-  //   play() {
-  //     osmd.FollowCursor = true;
-  //   },
-  //   pause() {},
-  //   reset() {},
-  //   bpmChanged() {},
-  //   volumeChanged() {},
-  //   volumeMute() {},
-  //   volumeUnmute() {}
-  // });
 
   timingSource.reset();
   timingSource.pause();
   timingSource.Settings = osmd.Sheet.playbackSettings;
   playbackManager.initialize(osmd.Sheet.musicPartManager);
   playbackManager.addListener(osmd.cursor);
-  //playbackManager.addListener(playbackControlPanel);
+
+  let lastEmittedMeasureIndex = 0;
+  playbackManager.addListener({
+    // 인터페이스 구현하지 않을 시 오류 발생 ( IPlaybackListener  전체 구현해야 정상 작동 )
+    cursorPositionChanged: (currentTimestamp, data) => {
+      const currentMeasureIndex = data.CurrentMeasureIndex;
+      if (currentMeasureIndex >= 0 && (currentMeasureIndex - lastEmittedMeasureIndex) >= 4) {
+        lastEmittedMeasureIndex = currentMeasureIndex;
+        emit('measure-changed', currentMeasureIndex);
+      }
+    },
+    resetOccurred: (data) => {
+      lastEmittedMeasureIndex = 0;
+    },
+    soundLoaded: (instrumentId, name) => {
+    },
+    allSoundsLoaded: () => {
+    },
+    pauseOccurred: () => {
+    },
+    selectionEndReached: () => {
+    },
+    notesPlaybackEventOccurred: () => {
+    },
+  });
+
   playbackManager.reset();
   osmd.PlaybackManager = playbackManager;
 };
 
 const play = () => {
-  console.log("play click")
+  console.log('play click');
   playbackManager.play();
 };
 
 const pause = () => {
-  console.log("pause click")
+  console.log('pause click');
   playbackManager.pause();
 };
 
 const stop = () => {
-  console.log("stop click")
+  console.log('stop click');
   playbackManager.pause();
   playbackManager.reset();
 };
@@ -92,24 +104,24 @@ const setVolume = () => {
   // 메트로놈 볼륨 등 추가로 재생 가능
   console.log('setVolume', volume.value);
   const instrumentIds = Array.from(playbackManager.instrumentIdMapping.keys());
-  instrumentIds.forEach(instrumentId => {
+  instrumentIds.forEach((instrumentId) => {
     playbackManager.volumeChanged(instrumentId, volume.value);
   });
 };
 
 onMounted(async () => {
   osmd = new window.opensheetmusicdisplay.OpenSheetMusicDisplay(osmdContainer.value);
-  console.log("osmd init");
+  console.log('osmd init');
   osmd.setOptions({
-    pageFormat:"A4 P", // P = 세로 L = 가로
-    pageBackgroundColor: "white"
-  })
+    pageFormat: 'A4 P', // P = 세로 L = 가로
+    pageBackgroundColor: 'white',
+  });
   const xml = await loadMusicXML();
-  console.log("loaded xml");
+  console.log('loaded xml');
   await osmd.load(xml);
-  console.log("osmd loaded xml");
+  console.log('osmd loaded xml');
   osmd.render();
-  console.log("osmd rendered");
+  console.log('osmd rendered');
   setupPlaybackManager();
 });
 </script>
@@ -118,15 +130,15 @@ onMounted(async () => {
 button {
   margin-right: 10px;
 }
-scrollContainer{
-  margin:0px;
-  padding : 0px;
+scrollContainer {
+  margin: 0px;
+  padding: 0px;
   width: 100%;
-  box-sizing:border-box;
+  box-sizing: border-box;
 }
-osmdContainer{
-  margin : 0px;
-  padding : 0px;
+osmdContainer {
+  margin: 0px;
+  padding: 0px;
   box-sizing: border-box;
 }
 </style>
