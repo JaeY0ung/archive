8840d7c64a0356c6caa2cf8ebb425e78a37be672
정재영 <hanol98@naver.com>
2024-07-25 16:53:06 +0900

    fix: 악보 띄우기 뷰 수정 및 리팩토링

--- frontend/src/common/sheet/SheetPage.vue
+++ frontend/src/common/sheet/SheetPage.vue
@@ -1,17 +1,29 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-
-const props = defineProps({
-  width: Number,
-  height: Number,
-});
+import { ref, onMounted, watch } from 'vue';
 
 const emit = defineEmits(['measure-changed']);
+
+const props = defineProps({
+  isPlay: {type: String, required : false, default: false},
+  width: {type: String, required : false, default: '100%'},
+  height: {type: String, required : false, default: '70vh'},
+})
+
+watch(() => props.isPlay, (newValue, oldValue) => {
+  if (newValue === 'play') {
+    play();
+  } else if (newValue === 'pause') {
+    pause();
+  } else if (newValue === 'stop') {
+    stop();
+  }
+})
 
 const osmdContainer = ref(null);
 const volume = ref(50); // 초기 볼륨 값을 50으로 설정
 let osmd = null;
 let playbackManager = null;
+
 
 const loadMusicXML = async(xmlFile) => {
   const response = await fetch(xmlFile);
@@ -71,24 +83,20 @@ const setupPlaybackManager = () => {
 };
 
 const play = () => {
-  console.log('play click');
   playbackManager.play();
 };
 
 const pause = () => {
-  console.log('pause click');
   playbackManager.pause();
 };
 
 const stop = () => {
-  console.log('stop click');
   playbackManager.pause();
   playbackManager.reset();
 };
 
 const setVolume = () => {
   // 메트로놈 볼륨 등 추가로 재생 가능
-  console.log('setVolume', volume.value);
   const instrumentIds = Array.from(playbackManager.instrumentIdMapping.keys());
   instrumentIds.forEach((instrumentId) => {
     playbackManager.volumeChanged(instrumentId, volume.value);
@@ -107,50 +115,31 @@ onMounted(async () => {
     });
   };
   document.body.appendChild(script);
-  // -----------------------------------------------------------------------------------
 
-  // osmd = new window.opensheetmusicdisplay.OpenSheetMusicDisplay(osmdContainer.value);
-  // console.log('osmd init');
-  // osmd.setOptions({
-  //   pageFormat: 'A4 P', // P = 세로 L = 가로
-  //   pageBackgroundColor: 'white',
-  // });
-
-  const xml = await loadMusicXML('/loa.musicxml'); console.log('loaded xml');
-  await osmd.load(xml); console.log('osmd loaded xml');
-  osmd.render(); console.log('osmd rendered');
+  const xml = await loadMusicXML('/loa.musicxml');
+  await osmd.load(xml); 
+  osmd.render();
   setupPlaybackManager();
 });
 </script>
 
 <template>
-  <div style="margin-left: 100px">
-    <div>
-      <button @click="play">재생하기</button>
-      <button @click="pause">일시정지</button>
-      <button @click="stop">처음으로</button>
-      <label for="volumeSlider">Volume: {{ volume }}</label>
-      <input id="volumeSlider" type="range" min="0" max="100" v-model="volume" @input="setVolume">
-    </div>
-    <div id="scrollContainer" style="overflow-y: scroll" :style="{ width, height }">
-      <div id="osmdContainer" ref="osmdContainer"></div>
-    </div>
+  <div class="w-full h-full min-h-full min-w-full">
+    <div class="osmd-container" :style="{'height':height, 'width':width}" ref="osmdContainer"></div>
   </div>
 </template>
 
 <style>
-button {
-  margin-right: 10px;
+.time-count {
+  font-size: 100px;
+  color:brown;
+  position: fixed;
+  top: 50vh;
+  left:50vw;
+  z-index: 50;
 }
-scrollContainer {
-  margin: 0px;
-  padding: 0px;
-  width: 100%;
-  box-sizing: border-box;
-}
-osmdContainer {
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
+
+.osmd-container {
+  overflow-y: hidden;
 }
 </style>
