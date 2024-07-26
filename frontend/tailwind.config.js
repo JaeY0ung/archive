e4a5d7c6acdc02ad9fc3daf000873699b8f88bcd
정재영 <hanol98@naver.com>
2024-07-26 16:41:34 +0900

    remove: 다크 모드 제거

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,39 +5,6 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      daisyui: {
-        themes: [
-          "light",
-          "dark",
-          "cupcake",
-          {
-            mytheme: {
-              
-              "primary": "#ff00ff",
-                        
-              "secondary": "#ff00ff",
-                        
-              "accent": "#00ffff",
-                        
-              "neutral": "#ff00ff",
-                        
-              "base-100": "#ff00ff",
-                        
-              "info": "#0000ff",
-                        
-              "success": "#00ff00",
-                        
-              "warning": "#00ff00",
-                        
-              "error": "#ff0000",
-            },
-          },
-        ],
-      },
-    },
-  },
   plugins: [require('daisyui')],
 }
 
