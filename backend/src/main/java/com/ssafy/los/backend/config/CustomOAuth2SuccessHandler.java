fe8b08d9ef59318128a8a5d53706ba3e4b940845
정재영 <hanol98@naver.com>
2024-07-29 11:03:23 +0900

    merge

--- backend/src/main/java/com/ssafy/los/backend/config/CustomOAuth2SuccessHandler.java
+++ backend/src/main/java/com/ssafy/los/backend/config/CustomOAuth2SuccessHandler.java
@@ -9,9 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -24,16 +24,20 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
     private final JWTUtil jwtUtil;
 
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
 
         CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         String email = customOAuth2User.getEmail();
-        
+
         // 회원가입
         if (customOAuth2User.getAuthorities().isEmpty()) {
             // TODO : 더 많은 정보를 회원가입에 제공하기
-            response.sendRedirect("http://localhost:5173/register?email=" + email);
+            response.sendRedirect(allowedOrigins + "/register?email=" + email);
         } else {
             // 로그인
             Collection<? extends GrantedAuthority> authorities = customOAuth2User.getAuthorities();
@@ -42,13 +46,13 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
             String role = authority.getAuthority();
 
             // JWT 토큰 발급
-            String token = jwtUtil.createJwt(email, role, 60*60*60L);
+            String token = jwtUtil.createJwt(email, role, 60 * 60 * 60L);
             log.info("OAuth2에서 임시 JWT를 발급한 뒤 쿠키에 담아 보냈습니다. = {} ", token);
 
             response.addCookie(createCookie("Authorization", token));
 
             // 프론트 엔드 주소 -> /auth/request
-            response.sendRedirect("http://localhost:5173/auth-success");
+            response.sendRedirect(allowedOrigins + "/auth-success");
         }
 
     }
@@ -56,7 +60,7 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     private Cookie createCookie(String key, String value) {
 
         Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(60*60*60);
+        cookie.setMaxAge(60 * 60 * 60);
         //cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
