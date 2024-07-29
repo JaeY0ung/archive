ce2277c7ee6b800ae79c13eadb188c551b992927
moongi <c880910@naver.com>
2024-07-29 15:50:38 +0900

    fix: user login시 로그인 실패나오는 오류 수정

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -19,21 +19,9 @@ const goToRegister = () => {
   router.push({ name: 'register' });
 }
 
-// const login = async () => {
-//     await userLogin(loginForm.value);
-//     if (isLogin.value) {
-//         const token = sessionStorage.getItem("accessToken");
-//         if (token) {
-//             await getUserInfo(token);
-//             router.replace({ name: "main" });
-//         }
-//     } else {
-//         alert("로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.");
-//     }
-// }
-
 const login = async () => {
     await userLogin(loginForm.value);
+    console.log('>>>>>>>>>>>', isLogin.value)
     if (isLogin.value) {
       router.replace({ name: "main" });
     } else {
