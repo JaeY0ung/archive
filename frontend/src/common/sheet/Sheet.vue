c21f6ad3bc86cc4f376ab080b9a11544c7510f51
신순호 <ssh9234@gmail.com>
2024-07-30 09:36:33 +0900

    refactor: 악보 컴포넌트 리팩토링

--- frontend/src/common/sheet/Sheet.vue
+++ frontend/src/common/sheet/Sheet.vue
@@ -1,144 +1,82 @@
-<template>
-  <div style="margin-left: 100px">
-    <div>
-      <button @click="play">재생</button>
-      <button @click="pause">일시정지</button>
-      <button @click="stop">정지</button>
-      <label for="volumeSlider">Volume: {{ volume }}</label>
-      <input id="volumeSlider" type="range" min="0" max="100" v-model="volume" @input="setVolume">
-    </div>
-    <div id="scrollContainer" style="overflow-y: scroll" :style="{ width: width + 'px', height: height + 'px' }">
-      <div id="osmdContainer" ref="osmdContainer"></div>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
+import Controller from "@/common/sheet/Controller.vue";
+import ScrollContainer from "@/common/sheet/ScrollContainer.vue";
+import RecordButton from "@/common/sheet/RecordButton.vue";
 
 const props = defineProps({
-  width: Number,
-  height: Number,
+  showController: Boolean,
+  isRecording: Boolean,
+  width: {
+    type: Number,
+    default: 800,
+  },
+  height: {
+    type: Number,
+    default: 600,
+  },
 });
-const emit = defineEmits(['measure-changed']);
 
-const osmdContainer = ref(null);
-const volume = ref(50); // 초기 볼륨 값을 50으로 설정
-let osmd = null;
-let playbackManager = null;
+const playbackManager = ref(null);
+const recordComponent = ref(null);
+const isPlay = ref(false);
+const triggerSplit = ref(0);
 
-const loadMusicXML = async () => {
-  const response = await fetch('/loa.musicxml');
-  const xml = await response.text();
-  return xml;
+const handleMeasureChanged = (measureIndex) => {
+  console.log("Measure changed:", measureIndex);
+  triggerSplit.value++;
 };
 
-const setupPlaybackManager = () => {
-  console.log('setup PlaybackManager');
+const musicStarted = () => {
+  console.log("Music started event");
+  isPlay.value = true;
+};
+
+const musicFinished = () => {
+  console.log("Music finished event");
+  isPlay.value = false;
+};
+
+const setVolume = (volume) => {
+  if (playbackManager.value) {
+    const instrumentIds = Array.from(playbackManager.value.instrumentIdMapping.keys());
+    instrumentIds.forEach((instrumentId) => {
+      playbackManager.value.volumeChanged(instrumentId, volume);
+    });
+  }
+};
+
+onMounted(() => {
   const timingSource = new window.opensheetmusicdisplay.LinearTimingSource();
-  console.log('init timingSource');
-  playbackManager = new window.opensheetmusicdisplay.PlaybackManager(
+  playbackManager.value = new window.opensheetmusicdisplay.PlaybackManager(
     timingSource,
     undefined,
     new window.opensheetmusicdisplay.BasicAudioPlayer(),
     undefined
   );
-  console.log('init playbackManager');
-  playbackManager.DoPlayback = true;
-  playbackManager.DoPreCount = false;
-  playbackManager.PreCountMeasures = 1;
-  osmd.FollowCursor = true;
-
-  timingSource.reset();
-  timingSource.pause();
-  timingSource.Settings = osmd.Sheet.playbackSettings;
-  playbackManager.initialize(osmd.Sheet.musicPartManager);
-  playbackManager.addListener(osmd.cursor);
-
-  let lastEmittedMeasureIndex = 0;
-  playbackManager.addListener({
-    // 인터페이스 구현하지 않을 시 오류 발생 ( IPlaybackListener  전체 구현해야 정상 작동 )
-    cursorPositionChanged: (currentTimestamp, data) => {
-      const currentMeasureIndex = data.CurrentMeasureIndex;
-      if (currentMeasureIndex >= 0 && (currentMeasureIndex - lastEmittedMeasureIndex) >= 4) {
-        lastEmittedMeasureIndex = currentMeasureIndex;
-        emit('measure-changed', currentMeasureIndex);
-      }
-    },
-    resetOccurred: (data) => {
-      lastEmittedMeasureIndex = 0;
-    },
-    soundLoaded: (instrumentId, name) => {
-    },
-    allSoundsLoaded: () => {
-    },
-    pauseOccurred: () => {
-    },
-    selectionEndReached: () => {
-    },
-    notesPlaybackEventOccurred: () => {
-    },
-  });
-
-  playbackManager.reset();
-  osmd.PlaybackManager = playbackManager;
-};
-
-const play = () => {
-  console.log('play click');
-  playbackManager.play();
-};
-
-const pause = () => {
-  console.log('pause click');
-  playbackManager.pause();
-};
-
-const stop = () => {
-  console.log('stop click');
-  playbackManager.pause();
-  playbackManager.reset();
-};
-
-const setVolume = () => {
-  // 메트로놈 볼륨 등 추가로 재생 가능
-  console.log('setVolume', volume.value);
-  const instrumentIds = Array.from(playbackManager.instrumentIdMapping.keys());
-  instrumentIds.forEach((instrumentId) => {
-    playbackManager.volumeChanged(instrumentId, volume.value);
-  });
-};
-
-onMounted(async () => {
-  osmd = new window.opensheetmusicdisplay.OpenSheetMusicDisplay(osmdContainer.value);
-  console.log('osmd init');
-  osmd.setOptions({
-    pageFormat: 'A4 P', // P = 세로 L = 가로
-    pageBackgroundColor: 'white',
-  });
-  const xml = await loadMusicXML();
-  console.log('loaded xml');
-  await osmd.load(xml);
-  console.log('osmd loaded xml');
-  osmd.render();
-  console.log('osmd rendered');
-  setupPlaybackManager();
 });
 </script>
 
-<style>
-button {
-  margin-right: 10px;
-}
-scrollContainer {
-  margin: 0px;
-  padding: 0px;
-  width: 100%;
-  box-sizing: border-box;
-}
-osmdContainer {
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
-}
-</style>
+<template>
+  <div>
+    <Controller
+      v-if="showController"
+      :playbackManager="playbackManager"
+      @music-started="musicStarted"
+      @set-volume="setVolume"
+    />
+    <ScrollContainer
+      :playbackManager="playbackManager"
+      :width="width"
+      :height="height"
+      @measure-changed="handleMeasureChanged"
+      @music-finished="musicFinished"
+    />
+    <RecordButton
+      v-if="isRecording"
+      ref="recordComponent"
+      :triggerSplit="triggerSplit"
+      :isPlay="isPlay"
+    />
+  </div>
+</template>
