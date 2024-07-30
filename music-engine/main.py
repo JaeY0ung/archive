d8bde5afd978f05d072e83838690abd44898166c
신순호 <ssh9234@gmail.com>
2024-07-31 02:00:44 +0900

    !hotfix: fastapi nginx 설정 변경

--- music-engine/main.py
+++ music-engine/main.py
@@ -4,12 +4,12 @@ from service.convert_service import ConvertService
 import os
 import shutil
 
-app = FastAPI()
+app = FastAPI(root_path="/fastapi")
 
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 특정 도메인만 허용 (예: http://localhost:3000)
+    allow_origins=["http://localhost:3000","http://localhost:8081","https://arc-hive:shop"],  # 특정 도메인만 허용 (예: http://localhost:3000)
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
