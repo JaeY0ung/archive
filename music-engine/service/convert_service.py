3256b30111f970d8b77f5239f0eb4ab341e0bd46
신순호 <ssh9234@gmail.com>
2024-07-31 09:46:49 +0900

    fix: fastapi omnizart volumn 설정

--- music-engine/service/convert_service.py
+++ music-engine/service/convert_service.py
@@ -16,12 +16,10 @@ class ConvertService:
         if ext != ".webm":
             raise ValueError("Only WebM files can be converted using this method")
 
-        # 변환된 파일의 경로 설정
         base_filename = os.path.splitext(os.path.basename(file_path))[0]
         wav_file = os.path.join(output_dir, f"{base_filename}.wav")
 
-        # ffmpeg를 사용하여 변환
-        subprocess.run(['ffmpeg','-y', '-i', file_path, wav_file], check=True)
+        subprocess.run(['ffmpeg', '-y', '-i', file_path, wav_file], check=True)
         return wav_file
 
     def wav_to_mp3(self, wav_file, output_dir="temp"):
@@ -33,10 +31,6 @@ class ConvertService:
                         '-ar', str(self.sample_rate), '-vn', mp3_file], check=True)
         return mp3_file
 
-    def webm_to_mp3(self, file_path, output_dir="temp"):
-        wav_file = self.convert_to_wav(file_path, output_dir)
-        mp3_file = self.convert_to_mp3(wav_file, output_dir)
-        return mp3_file
     def wav_to_midi(self, wav_file, output_dir="temp"):
         base_filename = os.path.splitext(os.path.basename(wav_file))[0]
         midi_file = os.path.join(output_dir, f"{base_filename}.mid")
@@ -44,8 +38,8 @@ class ConvertService:
         cmd = [
             "docker", "exec", "omnizart_container",
             "omnizart", "music", "transcribe",
-            wav_file, "-o", output_dir
+            os.path.join("/app/temp", os.path.basename(wav_file)),  # 절대 경로 사용
+            "-o", "/app/temp"
         ]
-        
         subprocess.run(cmd, check=True)
         return os.path.join(output_dir, f"{base_filename}.mid")
