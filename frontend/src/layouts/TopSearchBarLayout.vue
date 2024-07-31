55f01c21d940231243ecb538298a0b9dd2d93cde
moongi <c880910@naver.com>
2024-07-31 17:33:34 +0900

    feat: 검색 페이지 뷰 프론트 백엔드 연동

--- frontend/src/layouts/TopSearchBarLayout.vue
+++ frontend/src/layouts/TopSearchBarLayout.vue
@@ -1,10 +1,15 @@
 <script setup>
 import SearchBar from '@/common/search-bar/CommonSearchBar.vue'
+import {useRouter} from "vue-router";
+const router = useRouter();
+const searchSheetsByKeyword = (keyword)  => {
+  router.push({ name: 'sheetSearch', query: { keyword: keyword } });
+};
 </script>
 
 <template>
     <div class="h-full w-full">
-        <SearchBar class="search-bar "/>
+        <SearchBar class="search-bar " @send-keyword="searchSheetsByKeyword"/>
         <div style="height: 30px;"></div>
         <RouterView class="h-full w-full"/>
     </div>
--- frontend/src/views/SheetSearchView.vue
+++ frontend/src/views/SheetSearchView.vue
@@ -1,13 +1,100 @@
 <script setup>
+import {localAxios} from "@/util/http-common";
+import {onMounted, ref, watch} from "vue";
+import {useRoute} from "vue-router";
+import SmallSheetCard from "@/common/sheet/SmallSheetCard.vue";
 
+const route = useRoute();
+const local = localAxios();
+const sheets = ref([]);
+const keyword = ref('');
+
+const searchSheetsByKeyword =  ()  => {
+  console.log(keyword);
+  local.get(`/sheets`, {
+	params: { keyword: keyword.value }
+  })
+	  .then(({data}) => {
+		sheets.value = data;
+		sheets.value.map(s => s.songImg ? s.imageUrl = `data:image/jpeg;base64,${s.songImg}` : '기본 이미지');
+		console.log(data);
+	  })
+	  .catch((err) => alert(err));
+};
+
+watch(() => route.query.keyword,(newValue) => {
+  keyword.value = newValue || '';
+  searchSheetsByKeyword();
+})
+onMounted(() => {
+  keyword.value = route.query.keyword;
+  searchSheetsByKeyword();
+})
 </script>
 
 <template>
-    <div>
-        <h1>악보 검색 페이지</h1>
-    </div>
+
+  <div>
+    <h2>Search Results for </h2>
+    <div class="w-1/2 m-auto">
+	  <template v-if="sheets.length">
+		<div class="scroll-y flex-col">
+		  <SmallSheetCard v-for="sheet in sheets" :key="sheet.id" :sheet/>
+		</div>
+	  </template>
+	  <template v-else>
+		<p>No results found.</p>
+	  </template>
+	</div>
+  </div>
+
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  height: 100vh; /* 화면 전체 높이로 설정 */
+  margin: 10px 10px;
+  gap: 20px; /* 패널 간의 간격 설정 (예: 20px) */
+  width: 100%;
+}
 
+.box-div  {
+  padding: 10px;
+  background-color: rgb(255, 255, 255, 0.5);
+  border-radius: 15px;
+}
+
+.full-box {
+  width: 100%;
+}
+
+.recommend-box {
+  width: 70%;
+}
+
+.challenged-box {
+  width: 25%;
+}
+
+.up-div {
+  margin-bottom: 50px;
+}
+
+.down-div {
+  display: flex;
+  justify-content: space-between;
+}
+
+.left-panel, .right-panel {
+  padding: 20px;
+}
+.left-panel {
+  flex: 2; /* 왼쪽 패널은 전체의 2/10 */
+  background-color: #f0f0f0; /* 왼쪽 패널 배경색 */
+}
+.right-panel {
+  flex: 8; /* 오른쪽 패널은 전체의 8/10 */
+  background-color: #ffffff; /* 오른쪽 패널 배경색 */
+}
 </style>
