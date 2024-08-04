670848f582cfb408a3682e2ec3364b09f1318879
JaeY0ungJung <hanol5216@gmail.com>
2024-08-05 00:46:11 +0900

    add: 곡 장르 종류 모두 가져오는 기능 구현

---
+++ backend/src/main/java/com/ssafy/los/backend/controller/sheet/GenreController.java
@@ -0,0 +1,22 @@
+package com.ssafy.los.backend.controller.sheet;
+
+import com.ssafy.los.backend.service.sheet.GenreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genres")
+@RequiredArgsConstructor
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @GetMapping
+    public ResponseEntity<?> searchAllGenres() {
+        return new ResponseEntity<>(genreService.searchAllGenres(), HttpStatus.OK);
+    }
+}
---
+++ backend/src/main/java/com/ssafy/los/backend/service/sheet/GenreService.java
@@ -0,0 +1,9 @@
+package com.ssafy.los.backend.service.sheet;
+
+import com.ssafy.los.backend.domain.entity.Genre;
+import java.util.List;
+
+public interface GenreService {
+
+    List<Genre> searchAllGenres();
+}
