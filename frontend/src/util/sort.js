8354c9946728a9c4271bef66f1a9e80ee7c7768c
JaeY0ungJung <hanol5216@gmail.com>
2024-08-05 00:49:58 +0900

    refactor: tier와 sort, success status (성공, 실패 여부) 정보 리팩터링

---
+++ frontend/src/util/sort.js
@@ -0,0 +1,10 @@
+const sortInfo = [
+	{ value: "POPULAR", title: "인기순" },
+	{ value: "LATEST", title: "최신순" },
+	{ value: "HIGHEST_PLAY", title: "플레이순" },
+	{ value: "HIGHEST_STAR_RATE", title: "별점순" },
+	{ value: "HIGHEST_LEVEL", title: "난이도순" },
+	{ value: "HIGHEST_VIEW", title: "조회순"}
+]
+
+export { sortInfo };
---
+++ frontend/src/util/tier-info.js
@@ -0,0 +1,9 @@
+const tierInfo = [
+	{ level: 1, title: '브론즈' },
+	{ level: 2, title: '실버' },
+	{ level: 3, title: '골드' },
+	{ level: 4, title: '플래티넘' },
+	{ level: 5, title: '다이아' },
+]
+
+export { tierInfo };
