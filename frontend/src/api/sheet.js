29e079216aeaac9e764bf65f0a104691b3874756
JaeY0ungJung <hanol5216@gmail.com>
2024-08-05 00:52:47 +0900

    fix: sheet 가져올 때 에러 수정
    1. 인자 이름 (level -> levels 외 2개)
    2. 가져온 악보 데이터 없을 때, 예외 처리
    3. 성공 여부에 따라 가져오는 기능 구현 (80%)

--- frontend/src/api/sheet.js
+++ frontend/src/api/sheet.js
@@ -2,14 +2,20 @@ import { localAxios } from '@/util/http-common';
 
 const local = localAxios();
 
-async function searchSheetDetail (sheetId, success, fail) {
+async function searchSheetDetail(sheetId, success, fail) {
     await local.get(`/sheets/${ sheetId }`).then(success).catch(fail);
 }
-async function dislikeSheet (sheetId, success, fail) {
+
+async function searchSheetsByFilter(params, success) {
+    await local.get(`/sheets`, { params }).then(success).catch((err) => console.error(err));
+}
+
+async function dislikeSheet(sheetId, success, fail) {
     await local.delete(`/likes/sheets/${sheetId}`).then(success).catch(fail);
 }
 
 export {
     searchSheetDetail,
+    searchSheetsByFilter,
     dislikeSheet,
 };
