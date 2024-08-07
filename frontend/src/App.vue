a829a7e37d47a66f4fdf1898df12d83ee5d31fc4
신순호 <ssh9234@gmail.com>
2024-08-07 10:06:37 +0900

    fix: App.vue 마스터브랜치 롤백후 재푸쉬

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,32 +12,32 @@ const route = useRoute();
 
 <template>
     <div class="w-screen h-screen bg-gradient-to-br from-yellow-100 via-pink-200 to-blue-200">
-        <div class="absolute left-[-200px] flex h-screen bg-gradient-to-br" :class="{ 'left-0' : leftNavigationStore.navVisibility }"  style="transition: left 0.5s ease;">
-        <div>
-            <LeftNavigation class="h-screen w-[200px]"/>
-        </div>
+        <div class="absolute flex h-screen bg-gradient-to-br" :class="{ 'left-0' : leftNavigationStore.navVisibility, 'left-[-200px]': !leftNavigationStore.navVisibility }"  style="transition: left 0.5s ease;">
+            <div>
+                <LeftNavigation class="h-screen w-[200px]"/>
+            </div>
 
-        <div :class="{'w-screen' : !leftNavigationStore.navVisibility, 'w-screen-except-nav' : leftNavigationStore.navVisibility}">
-            <!-- 위 -->
-            <div style="height:50px; display:flex; justify-content:start;">
+            <div :class="{'w-screen' : !leftNavigationStore.navVisibility, 'w-screen-except-nav' : leftNavigationStore.navVisibility}">
+                <!-- 위 -->
+                <div style="height:50px; display:flex; justify-content:start;">
 
-                <div class="h-[50px] flex items-center cursor-pointer">
-                    <Hamburger class="w-[50px] h-50px max-h-[50px] flex justify-center items-center m-[5px]" @click="leftNavigationStore.changeNavVisibility"/>
-                    <RouterLink class="h-[25px] max-h-[50px] flex justify-center items-center" :to="{ name : 'main'}">
-                        <img class="mr-2" :src="require('@/assets/img/piano.svg')" alt="피아노">
-                        <div class="text-xl">악카이브</div>
-                    </RouterLink>
+                    <div class="h-[50px] flex items-center cursor-pointer">
+                        <Hamburger class="w-[50px] h-50px max-h-[50px] flex justify-center items-center m-[5px]" @click="leftNavigationStore.changeNavVisibility"/>
+                        <RouterLink class="h-[25px] max-h-[50px] flex justify-center items-center" :to="{ name : 'main'}">
+                            <img class="mr-2" :src="require('@/assets/img/piano.svg')" alt="피아노">
+                            <div class="text-xl">악카이브</div>
+                        </RouterLink>
+                    </div>
+                    
+                    <MyProfile style="height:40px;"/>
                 </div>
-                
-                <MyProfile style="height:40px;"/>
-            </div>
 
-            <!-- 아래 -->
-            <div class="router-view-parent" @click="leftNavigationStore.closeNav">
-                <RouterView :key="route.fullPath"/>
+                <!-- 아래 -->
+                <div class="router-view-parent" @click="leftNavigationStore.closeNav">
+                    <RouterView :key="route.fullPath"/>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </template>
 
