f62d23169b26f32a9e1731cdcc73167bb8168750
정재영 <hanol98@naver.com>
2024-08-13 16:00:18 +0900

    fix

--- frontend/src/api/sheet.js
+++ frontend/src/api/sheet.js
@@ -92,7 +92,6 @@ export {
     registerSheet,
     searchRecentPlayedsheet,
     updateSheet,
-    predictSheetLevel,
     getRecommendSheetByUserRecentPlay,
     deleteSheet,
     predictSheetLevel,
